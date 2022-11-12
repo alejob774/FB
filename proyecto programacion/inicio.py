cd208f5ec862bfcabd7cd11879dc27c14aba7e65
geronimorojasg <115667638+geronimorojasg@users.noreply.github.com>
2022-11-11 22:36:57 -0500

    Add files via upload

--- proyecto programacion/inicio.py
+++ proyecto programacion/inicio.py
@@ -5,20 +5,20 @@ from pygame.locals import *
 size_y = 750
 size_x = (size_y*(3.25/5))
 superficie=pygame.display.set_mode((size_x,size_y))#pantalla (pixeles)
-fondo = pygame.image.load("fondo.png").convert()
+fondo = pygame.image.load(r"C:\Users\Jeronimo\OneDrive\Escritorio\PROGRAMACIÓN\fondo.png").convert()
 fondo=pygame.transform.scale(fondo, (size_x,size_y))
-flappy = pygame.image.load("flappy.png").convert_alpha()
+flappy = pygame.image.load(r"C:\Users\Jeronimo\OneDrive\Escritorio\PROGRAMACIÓN\flappy.png").convert_alpha()
 flappy=pygame.transform.scale(flappy, (450,450))
-start = pygame.image.load("start.png").convert_alpha()
+start = pygame.image.load(r"C:\Users\Jeronimo\OneDrive\Escritorio\PROGRAMACIÓN\start.png").convert_alpha()
 start=pygame.transform.scale(start, (200,200))
 
-arriba=pygame.image.load("arriba.png").convert_alpha() #optimiza la imagen con los mismos pixeles para que no se trabe
+arriba=pygame.image.load(r"C:\Users\Jeronimo\OneDrive\Escritorio\PROGRAMACIÓN\arriba.png").convert_alpha() #optimiza la imagen con los mismos pixeles para que no se trabe
 arriba=pygame.transform.scale(arriba,(80,80))
 
-medio=pygame.image.load("medio.png").convert_alpha()
+medio=pygame.image.load(r"C:\Users\Jeronimo\OneDrive\Escritorio\PROGRAMACIÓN\medio.png").convert_alpha()
 medio=pygame.transform.scale(medio,(80,80))
 
-abajo=pygame.image.load("abajo.png").convert_alpha()
+abajo=pygame.image.load(r"C:\Users\Jeronimo\OneDrive\Escritorio\PROGRAMACIÓN\abajo.png").convert_alpha()
 abajo=pygame.transform.scale(abajo,(80,80))
 
 imagenes=[arriba,medio,abajo,medio]
@@ -38,8 +38,10 @@ def main():
                 run=False
         
             if evento.type==pygame.MOUSEBUTTONDOWN:
-                if pygame.Rect.colliderect():
-                    break
+                if 168 <= mouse[0] <= 319 and 540 <= mouse[1] <= 635: #muestra la ubicacion exacta del boton de start
+                    pygame.quit()
+                
+        mouse = pygame.mouse.get_pos() #yuda a tener la ubicacion exacta del mouse
 
         superficie.blit(fondo,(0,0)) #foto de fondo sale en pantalla
         superficie.blit(flappy,(55,40)) #foto del pajaro
